12b13e6d9ba042f3027cedcff4dc2471627ffc64
BreakerSneaker <brann1802@gmail.com>
2023-09-15 13:49:47 +0300

    index

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
     window.open("https://breakersneaker.github.io/portfolioMobile/");
-  }else{
-    // false for not mobile device
-    window.open('index.html');
   }
